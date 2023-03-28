40aacb9516756768b81bd278fe731107d8712258
Hem Pun <hemthegentlmen@gmail.com>
2023-03-28 18:38:25 +0545

    Learned About use Effect with Chats example

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './style.css'
 import Resturant from './components/Resturant'
 import UseState from './components/Hooks/UseState'
+import UseEffect from './components/Hooks/UseEffect'
 
 function App() {
   return (
     // <Resturant/>
-    <UseState/>
+    // <UseState/>
+    <UseEffect/>
   )
 }
 
